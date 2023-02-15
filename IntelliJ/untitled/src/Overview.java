eedab5dec8d9fa048ecd32495c3ccf7c5d53863a
Panthaweekan <95071144+Panthaweekan@users.noreply.github.com>
2023-02-15 14:04:14 +0700

    Sample Overview

---
+++ IntelliJ/untitled/src/Overview.java
@@ -0,0 +1,17 @@
+public interface Overview {
+    public interface Player{
+
+    }
+    public interface Territory{
+
+    }
+    public interface Region{
+
+    }
+    public interface City{
+
+    }
+    public interface CityCrew{
+
+    }
+}
