27e36cafcfef91a81520fe5e5a502145c9ff5f6d
napatsiri <nnapatsiri@gmail.com>
2023-02-25 14:35:52 +0700

    add to click card

--- Frontend/seleccha.js
+++ Frontend/seleccha.js
@@ -1,18 +1,25 @@
-var card__background = document.getElementById("image1");
-var card__background = document.getElementById("image2");
+// Get references to the cards and add click event listeners
+const card1 = document.querySelector('.card:nth-of-type(1)');
+const card2 = document.querySelector('.card:nth-of-type(2)');
 
-card__background.addEventListener("click", function() {
-  // remove "selected" class from other image
-  icard__background.classList.remove("selected");
-  
-  // add "selected" class to this image
-  card__background.classList.add("selected");
+card1.addEventListener('click', () => {
+  // Player 1 card clicked
+  localStorage.setItem('player', '1'); // Store player selection in local storage
+  card1.classList.add('selected'); // Add selected class to card
+  card2.classList.remove('selected'); // Remove selected class from other card
 });
 
-card__background.addEventListener("click", function() {
-  // remove "selected" class from other image
-  card__background.remove("selected");
-  
-  // add "selected" class to this image
-  card__background.add("selected");
+card2.addEventListener('click', () => {
+  // Player 2 card clicked
+  localStorage.setItem('player', '2'); // Store player selection in local storage
+  card2.classList.add('selected'); // Add selected class to card
+  card1.classList.remove('selected'); // Remove selected class from other card
 });
+
+// Check local storage for previous player selection
+const playerSelection = localStorage.getItem('player');
+if (playerSelection === '1') {
+  card1.classList.add('selected'); // Add selected class to player 1 card
+} else if (playerSelection === '2') {
+  card2.classList.add('selected'); // Add selected class to player 2 card
+}
